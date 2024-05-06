7ec99c029f7009b0e75bb4d5ac1536400c0f0528
Astridd06 <168500994+Astridd06@users.noreply.github.com>
2024-05-05 20:19:28 -0600

    Delete librojava.js

--- librojava.js
+++
@@ -1,7 +0,0 @@
-const flipbook = document.getElementById('flipbook');
-let currentPage = 0;
-
-flipbook.addEventListener('click', () => {
-  currentPage = (currentPage + 1) % 3;
-  flipbook.style.transform = `translateX(-${currentPage * 50}%)`;
-});
